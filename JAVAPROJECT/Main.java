8cb575566a70836e9ff64ba690d0fe18cfaae020
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-27 13:51:22 +0100

    Add files via upload

---
+++ JAVAPROJECT/Main.java
@@ -0,0 +1,11 @@
+import JAVAPROJECT.CLASSES.Dice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // initialize Dice constructor
+        Dice dice = new Dice();
+        dice.generateDice();
+    }
+
+}
