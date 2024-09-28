40b93eb0d26f413c78b5b10b1867c401e9a31117
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-28 15:53:06 +0100

    Add files via upload

--- JAVAPROJECT/CLASSES/Player.java
+++ JAVAPROJECT/CLASSES/Player.java
@@ -1,5 +1,50 @@
 package JAVAPROJECT.CLASSES;
 
-public class player {
+public class Player {
+
+    // create variables for differnet scores?
+    // maybe I should create a differnt class for the scores... ask
+    int aces;
+    int twos;
+    int threes;
+    int fours;
+    int fives;
+    int sixes;
+    int totalScore;
+
+    // Max number of rerolls
+    int maxReroll = 2;
+
+    // initialize Dice constructor
+    Dice dice = new Dice();
+
+    public Player(String name) {
+        aces = 0;
+        twos = 0;
+        threes = 0;
+        fours = 0;
+        fives = 0;
+        sixes = 0;
+        totalScore = 0;
+
+        turn();
+    }
+
+    public void turn() {
+        // function to representa turn, ideally will be called for each player back to
+        // back for a set number of turns
+        dice.generateDice();
+        dice.keepDice();
+        System.out.println(aces);
+
+        // if the reroll index is lower than max, reroll and call keepDice
+        for (int rerollNumber = 0; rerollNumber < maxReroll; rerollNumber++) {
+            System.out.println("Reroll number " + (rerollNumber + 1) + " out of 2");
+            dice.rerollDice();
+            dice.keepDice();
+        }
+
+        System.out.println("TURN FINISHED");
+    }
 
 }
