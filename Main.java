29986a025fe258e8cb0cf691ab1742cc0d161e04
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-29 19:12:03 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package JAVAPROJECT;
+
+import JAVAPROJECT.CLASSES.Player;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // initialize Dice constructor
+        Player blue = new Player("Blue");
+        Player julia = new Player("Julia");
+        Player carl = new Player("Carl");
+
+    }
+
+}
