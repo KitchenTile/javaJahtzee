3644544bdf9e94ebb59a2338102e03ee9fa76766
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-10-01 16:59:39 +0100

    Add files via upload

--- JAVAPROJECT/CLASSES 2/Game.java
+++ JAVAPROJECT/CLASSES 2/Game.java
@@ -13,7 +13,13 @@ public class Game {
 
     Player[] playersList = { player1, player2, player3 };
 
-    int numberOfTurns = 12;
+    int winner;
+
+    int NUMBEROFTURNS = 12;
+
+    int XCHAR = -1;
+
+    boolean[] firstTurnArray = new boolean[11];
 
     // initializes as 12 falses. I'll use them to accurately check arrays for scores
     boolean[] takenColumn = new boolean[11];
@@ -25,40 +31,54 @@ public class Game {
         int[] player2list = player2.getDiceRollArray();
         int[] player3list = player3.getDiceRollArray();
 
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 11; i++) { // If there's a clear winner but the other two are a draw, it will draw
             // check that all the values of the column are taken and that column has not
             // been evaluated before
+
             if (player1list[i] != 0 && player2list[i] != 0
                     && player3list[i] != 0 && !takenColumn[i]) {
-                // if two players have the same roll in a column, discard the points
+                // if two players have the same roll in a column, discard the points. If there's
+                // a max give points to the max regardless of the tie
                 if (player1list[i] == player2list[i] || player1list[i] == player3list[i]
                         || player2list[i] == player3list[i]) {
-                    System.out.println("column " + i + " tie -- 0 points");
+                    // if there is some instance of a tie, check if there's a max
+                    if (player1list[i] > player2list[i] && player1list[i] > player3list[i]) {
+                        player1.totalScore += (i + 2);
+                        player2list[i] = XCHAR;
+                        player3list[i] = XCHAR;
+                    } else if (player2list[i] > player1list[i] && player2list[i] > player3list[i]) {
+                        player2.totalScore += (i + 2);
+                        player1list[i] = XCHAR;
+                        player3list[i] = XCHAR;
+                    } else if (player3list[i] > player2list[i] && player3list[i] > player1list[i]) {
+                        player3.totalScore += (i + 2);
+                        player2list[i] = XCHAR;
+                        player1list[i] = XCHAR;
+                    } else {
+                        System.out.println("column " + i + " tie -- 0 points");
+                    }
+
                     // if either player has a higher roll, then give the points to the player
                 } else if (player1list[i] > player2list[i] && player1list[i] > player3list[i]) {
-                    player1.totalScore += (i + 1);
+                    player1.totalScore += (i + 2);
+                    player2list[i] = XCHAR;
+                    player3list[i] = XCHAR;
                     System.out.println("column " + (i + 2) + " winner is: " + player1.name);
                 } else if (player2list[i] > player1list[i] && player2list[i] > player3list[i]) {
-                    player2.totalScore += (i + 1);
+                    player2.totalScore += (i + 2);
+                    player1list[i] = XCHAR;
+                    player3list[i] = XCHAR;
                     System.out.println("column " + (i + 2) + " winner is: " + player2.name);
                 } else {
-                    player3.totalScore += (i + 1);
+                    player3.totalScore += (i + 2);
+                    player2list[i] = XCHAR;
+                    player1list[i] = XCHAR;
                     System.out.println("column " + (i + 2) + " winner is: " + player3.name);
 
                 }
                 takenColumn[i] = true;
             }
         }
-
-    }
-
-    public void checkFirstTurn() {
-        if (playersList[1].firstColumnIndex == playersList[2].firstColumnIndex
-                || playersList[1].firstColumnIndex == playersList[3].firstColumnIndex
-                || playersList[2].firstColumnIndex == playersList[3].firstColumnIndex) {
-            System.out.println("ERROR NOT UNIQUE COLUMN ON THE FIRST ROLL");
-        }
-        ;
 
     }
 
@@ -78,6 +98,19 @@ public class Game {
         }
         System.out.println("----------------------------------------------");
         System.out.println("----------------------------------------------");
+    }
+
+    public void compareTotalScores() {
+        int topScore = 0;
+
+        for (int playerIndex = 0; playerIndex < playersList.length; playerIndex++) {
+            if (playersList[playerIndex].totalScore > topScore) {
+                topScore = playersList[playerIndex].totalScore;
+                winner = playerIndex;
+            }
+        }
+
+        System.out.println("The winner is " + playersList[winner].name + "with a score of " + topScore + "!");
 
     }
 
@@ -85,25 +118,17 @@ public class Game {
 
         // for the duration of a game play each player's turn -- need to add first turn
         // restriction
-        for (int currentTurn = 0; currentTurn < numberOfTurns; currentTurn++) {
-
-            if (currentTurn = 0) {
-                for (int playerIndex = 0; playerIndex < playersList.length; playerIndex++) {
-                    System.out.println(playersList[playerIndex].name + "'s Turn");
-                    playersList[playerIndex].turnOne();
-                    checkFirstTurn();
-                }
-            }
+        for (int currentTurn = 0; currentTurn < (NUMBEROFTURNS - 1); currentTurn++) {
             for (int playerIndex = 0; playerIndex < playersList.length; playerIndex++) {
                 System.out.println(playersList[playerIndex].name + "'s Turn");
-                playersList[playerIndex].turn();
+                playersList[playerIndex].turn(firstTurnArray);
             }
             checkArrays();
-
-            System.out.println("Turn number " + (currentTurn + 1) + " over...");
+            // I add two here to keep up with the for loop stating on turn 2. -redo
+            System.out.println("Turn number " + (currentTurn + 2) + " over...");
             gameTable();
-
         }
+        compareTotalScores();
 
     }
 
