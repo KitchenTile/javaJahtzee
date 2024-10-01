db1e405023c6fc797be507a2035626f3fccc1268
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-10-01 10:15:32 +0100

    Add files via upload

--- JAVAPROJECT/Main.java
+++ JAVAPROJECT/Main.java
@@ -1,15 +1,16 @@
 package JAVAPROJECT;
 
+import java.util.Arrays;
+
+import JAVAPROJECT.CLASSES.Game;
 import JAVAPROJECT.CLASSES.Player;
 
 public class Main {
 
     public static void main(String[] args) {
-        // initialize Dice constructor
-        Player blue = new Player("Blue");
-        Player julia = new Player("Julia");
-        Player carl = new Player("Carl");
+        Game game = new Game();
 
+        game.play();
     }
 
 }
