903c105fd2d8ec125efb5c90ab8ba49913021942
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-10-01 10:15:21 +0100

    Add files via upload

--- JAVAPROJECT/CLASSES 2/Player.java
+++ JAVAPROJECT/CLASSES 2/Player.java
@@ -24,12 +24,18 @@ public class Player {
     // initialize Dice constructor
     Dice dice = new Dice();
 
+    // scanner to assign the value to the selected index
+    Scanner input;
+
     String name;
+
+    int firstColumnIndex;
 
     public Player() {
         totalScore = 0;
-        diceRollArray = new int[12];
+        diceRollArray = new int[11];
         turnOne = true;
+        // int firstColumnIndex = null;
 
         Scanner nameInput = new Scanner(System.in); // needs closing
         System.out.print("Please enter your name: ");
@@ -49,8 +55,7 @@ public class Player {
         // while loop to make sure the selected
         while (!unselectedValidColumn /* && !turnOne */) {
 
-            // scanner to assign the value to the selected index
-            Scanner input = new Scanner(System.in);
+            input = new Scanner(System.in);
             System.out.print("What column would you like to assing this value to? ");
             int columnIndex = input.nextInt();
 
@@ -58,10 +63,10 @@ public class Player {
             // the index has to be bigger or equal than 0,
             // the index has to be smaller or equal than the length of the array,
             // and the element in the array has to be untaken (equals to 0)
-            if (columnIndex > 0 && columnIndex <= diceRollArray.length && diceRollArray[columnIndex - 1] == 0) {
+            if (columnIndex > 1 && columnIndex <= diceRollArray.length && diceRollArray[columnIndex - 2] == 0) {
 
                 // adjust for a 0 index array by subtracting 1
-                diceRollArray[columnIndex - 1] = dice.getRollValue();
+                diceRollArray[columnIndex - 2] = dice.getRollValue();
 
                 // switch variable to true
                 unselectedValidColumn = true;
@@ -72,12 +77,42 @@ public class Player {
         }
 
         System.out.println(Arrays.toString(diceRollArray));
+    }
 
-        // chewck if it's the first turn to ask for an untaken cell.
-        // if (turnOne) {
-        // System.out.println("What column would you like to assing this value to?
-        // (COLUMN MUST BE UNIQUE)");
-        // }
+    public void turnOne() {
+
+        unselectedValidColumn = false;
+
+        // call the function to roll dice and get the added value
+        dice.generateDice();
+        System.out.println("You rolled a(n) " + dice.getRollValue() + " this turn.");
+
+        // while loop to make sure the selected
+        while (!unselectedValidColumn /* && !turnOne */) {
+
+            input = new Scanner(System.in);
+            System.out.print("What column would you like to assing this value to? ");
+            int columnIndex = input.nextInt();
+            firstColumnIndex = input.next();
+
+            // three conditions for the column to be valid:
+            // the index has to be bigger or equal than 0,
+            // the index has to be smaller or equal than the length of the array,
+            // and the element in the array has to be untaken (equals to 0)
+            if (columnIndex > 1 && columnIndex <= diceRollArray.length && diceRollArray[columnIndex - 2] == 0) {
+
+                // adjust for a 0 index array by subtracting 1
+                diceRollArray[columnIndex - 2] = dice.getRollValue();
+
+                // switch variable to true
+                unselectedValidColumn = true;
+            } else {
+                System.out.println("Please enter a valid column number.");
+            }
+
+        }
+
+        System.out.println(Arrays.toString(diceRollArray));
     }
 
     public int[] getDiceRollArray() {
